56275e8940d5df981f71cb954db820c8c8d2d6c2
werrik15-a11y <werrik15@gmail.com>
2026-09-26 02:30:55 +0300

    script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 // ==========================================
 
 // ✅ 100% РАБОЧАЯ ССЫЛКА: Прямой экспорт CSV из вашей таблицы (на основе её ID)
-const GOOGLE_SHEET_CSV_URL = "https://google.com";
+const GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTivUu_TjZphuspUCedVqZnmSLpgFTZDfVDnnvln6WapFSvekyKm-8UMukuaOfQ0VIbK_zOs5xeWEJD/pubhtml?gid=0&single=true";
 
 // ✅ Ваша рабочая ссылка на Apps Script для фоновой записи проставляемых галок
-const GOOGLE_SCRIPT_WEB_APP_URL = "https://google.com";
+const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxWstQD5cngpAwHF-UrbTneMB6o_g7A_eryrcF_SOhGHwIfsnc6QPettXV6zQxUIWQo/exec";
 
 // 🔐 Пароль лидера для входа в панель управления
 const ADMIN_PASSWORD = "UNI_LEADER_2026";
